32168438bbd5a6d89fd44ceaf51a975c4b7301d8
Akshita <akshitaag0708@gmail.com>
2026-03-06 13:06:14 +0530

    adding notes and class task of 6 march

---
+++ Class Notes/6march.py
@@ -0,0 +1,123 @@
+# global vs local scope
+
+# a=10
+# def i():
+#     a=9
+#     print(a)
+# i()          # output will be 9 
+# print(a)     #output will be 10 as a=9 is in local scope 
+# ----------------------------------------------------------
+# global keyword
+
+# a=10
+# def i():
+#     global a
+#     a=9
+#     print(a)
+# i()          # output will be 9 
+# print(a)     #output will be 9 as a=9 is global in local scope 
+# ----------------------------------------------------------------------
+# nonlocal keyword
+
+# def i():
+#     b=9
+#     print(b)
+#     def j():
+#         b=5
+#         print(b)
+#     j()       #o/p = 9
+# i()      #o/p =5
+
+
+# def i():
+#     b=9
+#     print(b)
+#     def j():
+#         nonlocal b 
+#         b=5
+#         print(b)
+#     j()       #o/p = 9
+# i()      #o/p =5
+
+# -----------------------------------------------------------------------------
+
+# # task1  product of  list elements
+# def pro():
+#     a=eval(input("enter list"))
+#     p=1
+#     for i in a:
+#         p=p*int(i)
+#     return p
+# result=pro()
+# print(result)
+
+# ---------------------------------------------------------------------
+# # task 2:  wap to print initial index of char present in given string
+ 
+# def ind(name , letter):
+#     for i in range(len(name)):
+#             if name[i]==letter:
+#                 return i
+# print(ind(input("enter str"),input("enter char")))
+
+# ----------------------------------------------------------------------
+
+# packing and unpacking:
+
+# def ind(s , ch):
+#     for i in range(len(s)):
+#         if s[i]==ch:
+#             return i
+# print(ind('aksh','i','t'))       #gives error
+
+# def ind(v,t):
+#     for i in range(len(t)):
+#         if t[i]==v:
+#             return i
+# print(ind(100,20,30,20,30,100))     #error : ind() takes 2 positional arguments but 6 were given
+
+# ---------------------------------------------------------------------------
+
+# tuple packing
+# def ind(v,*t):
+#     for i in range(len(t)):
+#         if t[i]==v:
+#             return i
+# print(ind(100,20,30,20,30,100))     #o/p =4 v ke baad ki jo bhi values hogi vo t m store ho jaegi as a tuple
+
+# Note : we need to pass packing argument as last argument otherwise it gives error
+
+# def ind(*t,v):
+#     for i in range(len(t)):
+#         if t[i]==v:
+#             return i
+# print(ind(100,20,30,20,30,100))    # gives error bcz tuple arg is passed first with *
+
+# ------------------------------------------------------------------------------
+
+# dictionary packing
+
+# def dic(d):
+#     return d
+# print(dic(a=100,b=200,c=30,d=70))      #error 
+
+
+# def dic(*d):
+#     return d
+# print(dic(a=100,b=200,c=30,d=70))   # error 
+
+
+# def dic(**d):
+#     return d
+# print(dic(a=100,b=200,c=30,d=70))     # o/p={'a': 100, 'b': 200, 'c': 30, 'd': 70}
+
+# def dic(**d):
+#     return d
+# print(dic(a=100,b=200,12='aksh',d=70))  # number can not be key
+
+
+def dic(**d):
+    return d
+print(dic(a=100,b=200,c1=30,d=70))  #o/p={'a': 100, 'b': 200, 'c1': 30, 'd': 70}
+#  keys here are like variable so must follow those 6 rules of identifier
+# -----------------------------------------------------------------------
