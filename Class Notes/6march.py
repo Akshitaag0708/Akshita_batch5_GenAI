b77434634981fcfcb20a6ad6d26897274362047c
Akshita <akshitaag0708@gmail.com>
2026-03-06 13:34:05 +0530

    adding notes and class task of 6 march

--- Class Notes/6march.py
+++ Class Notes/6march.py
@@ -116,8 +116,41 @@
 # print(dic(a=100,b=200,12='aksh',d=70))  # number can not be key
 
 
-def dic(**d):
-    return d
-print(dic(a=100,b=200,c1=30,d=70))  #o/p={'a': 100, 'b': 200, 'c1': 30, 'd': 70}
+# def dic(**d):
+#     return d
+# print(dic(a=100,b=200,c1=30,d=70))  #o/p={'a': 100, 'b': 200, 'c1': 30, 'd': 70}
 #  keys here are like variable so must follow those 6 rules of identifier
+
 # -----------------------------------------------------------------------
+
+# unpacking of tuple
+# def addition(a, b, c):
+#     return a + b + c
+# num = (1, 5, 10)  
+# print( addition(*num) )     #o/p=16 ,Explanation: *num unpacks numbers into a, b, c.
+
+
+# def p(a,b,c,d):
+#     print(a,b,c,d)
+# s='wxyz'
+# p(*s)       #o/p = w x y z   ,here every index element of string is assigned with a , b , c , ressp.
+# --------------------------------------------------------------------------
+
+# unpacking of dictionary
+
+# def info(name, age, country):
+#     print(f"Name: {name}, Age: {age}, Country: {country}")
+
+# data = {"name": "akshita", "age": 30, "country": "India"}
+# info(**data) 
+# only values will be printed 
+# o/p =Name: akshita, Age: 30, Country: India
+# Explanation: **data unpack dictionary values and assign them to parameters.
+
+
+
+# def info(name, age, country):
+#     print(name,age,country)
+
+# data = {"name": "akshita", "age": 30, "country": "India"}
+# info(**data)     # o/p = akshita 30 India   , only values will be printed
